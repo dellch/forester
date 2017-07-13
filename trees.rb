1c16340f417f387788b4629676ad92b5e8589d67
dellch <cdell@ufl.edu>
2017-07-13 11:30:29 -0400

    switched regex to a variable for reuse

--- trees.rb
+++ trees.rb
@@ -23,7 +23,8 @@ unless filename.nil?
   num_chars = array.delete_at(0).split(' ')[1].to_i
   begin_char_index = array[0].index(/(?<=\s)[0-9?]/)
   last_char_index = begin_char_index + num_chars
-  out = array.pop.match(/[A-Za-z_\(\)0-9]+/)[0]
+  taxon_regex = /[A-Za-z_\(\)0-9]+/
+  out = array.pop.match(taxon_match)[0]
   puts 'running...'
   puts "#{num_chars.to_s} characters to process"
   #from first character, to last character
@@ -37,9 +38,9 @@ unless filename.nil?
       next if spot == '?' || spot == ' '  #ignore ? spots
       case line[index]
         when '1'
-          ones.push(line.match(/[A-Za-z_\(\)0-9]+/)[0])
+          ones.push(line.match(taxon_regex)[0])
         when '0'
-          zeros.push(line.match(/[A-Za-z_\(\)0-9]+/)[0])
+          zeros.push(line.match(taxon_regex)[0])
       end
     end
 
