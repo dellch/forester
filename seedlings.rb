30cd45ae2d4bedd9508609b81002ef9cf0c99a61
dellch <cdell@ufl.edu>
2017-07-13 12:23:47 -0400

    working on incremental file writing

--- seedlings.rb
+++ seedlings.rb
@@ -12,54 +12,50 @@ filename = original_file
 unless filename.nil?
   file = File.open(filename)
   str = file.read
-  array = str.split(/\n/)
-  out, out_chars, new_file_contents = "","",""
+  lines = str.split(/\n/)
+  new_filename = "altered_#{filename}.txt"
   #get rid of count line which is first line
-  num_chars = array.delete_at(0).split(' ')[1].to_i
-  begin_char_index = array[0].index(/(?<=\s)[0-9?]/)
-  last_char_index = begin_char_index + num_chars  
-  out = array.pop.match(/[A-Za-z_\(\)0-9]+/)[0]
-  puts 'running...'  
-  puts "#{num_chars.to_s} characters to process"
-  #array is now OTU     ####### format
+  num_chars = lines.delete_at(0).split(' ')[1].to_i
+  begin_char_index = lines[0].index(/(?<=\s)[0-9?]/)
+  last_char_index = begin_char_index + num_chars
+  taxon_regex = /[A-Za-z_\(\)0-9]+/
+  outgroup_index = lines.find_index{ |line| line[begin_char_index..last_char_index].match(/^0*$/) rescue false }
+  outgroup = lines[outroup_index].match(taxon_regex)[0]
+  five_percent_of_characters = (1.0*num_chars/20/100).floor*100
+
+  puts 'running...'
+  puts 'Getting outgroup taxon...'
+  puts "#{outgroup} is the outgroup taxon"
+  puts "processing file with #{num_chars.to_s} characters (#{lines.length.to_s} taxa)..."
+  
   (begin_char_index..last_char_index).each do |index|
-    ones = []
-    zeros = []
+    ones, zeros = [], []
     
-    array.each do |line|
+    lines.each do |line|
       spot = line[index]
-      next if spot=="?" || spot == " "
+      next if spot =='?' || spot == ' '
       case line[index]
         when '1'
-          ones.push(line.match(/[A-Za-z_\(\)0-9]+/)[0])
+          ones.push(line.match(taxon_regex)[0])
         when '0'
-          zeros.push(line.match(/[A-Za-z_\(\)0-9]+/)[0])
+          zeros.push(line.match(taxon_regex)[0])
       end
-  end
-  otu_chars_hash = {}
-  new_filename = "altered_"+filename+".txt"
-
-  array.each do |line|
-    otu = line.match(/[A-Za-z_\(\)0-9]+/)[0]
-    chars = line.match(/(?<=\s)[0-9?]+/)[0].split('')
-    out, out_chars = otu, chars if chars.all?{|chr|chr=="0"}
-    otu_chars_hash[otu] = chars unless chars.all?{|chr|chr=="0"}
-  end
-
-  out_chars.each_with_index do |character, index|
-    puts "SOMETHING IS WRONG WITH THE SCRIPT" if character != "0"
-    ones = []
-    zero = []
-    otu_chars_hash.each_key do |key|
-      ones.push(key) if otu_chars_hash[key][index] == "1"
-      zero.push(key) if otu_chars_hash[key][index] == "0"
     end
-    puts "MORE THAN ONE 0" if zero.length > 1
-    out = "(#{out},(#{zero.first},(#{ones[0]},#{ones[1]})));\r\n"
-    File.open(new_filename, 'a'){|f| f.puts(out)}    
-    puts puts "lines processed: #{index.to_s} out of #{num_chars.to_s}"  if index%1000 == 0
+
+
+    unless ones.empty? and zeros.empty?
+      puts "MORE THAN ONE 0s for #{outgroup}" if zeros.length > 1
+      puts "MORE THAN TWO 1s for #{outgroup}" if ones.length > 2
+      File.open(new_filename, 'a'){ |f| f.puts("#{outgroup},(#{zeros[0]},(#{ones[0]},#{ones[1]})));\r\n") }
+    end
+
+    if index%five_percent_of_characters == 0
+      percent_complete = (100.0*index/num_chars).floor
+      puts "characters processed: #{index.to_s} of #{num_chars.to_s}"
+      puts percent_complete.to_s + '% complete'
+    end
   end
-  puts "NEW FILE CREATED:  #{new_filename}"
+  puts "Processing complete\r\nNEW FILE CREATED:  #{new_filename}"
 else
   puts "ALERT: You must declare a filename:  usage should be \"ruby evgeny.rb NAME_OF_FILE\""
 end
